00c88461b20f71df8027c071bc9b2af630512aee
Rajnit Rai <dipakshrestha475@gmail.com>
2022-12-13 17:49:06 +0545

    added file

--- hello.js
+++ hello.js
@@ -1,1 +1,47 @@
-console.log("hello world")
+//Variables
+//Declaring Variables
+//var a=1;
+//var: global scope ,
+//var b=4;
+//console.log(a+b)
+//types of variable
+//var a=1;
+//let= cannot redeclare
+//let b=7;
+//let a=5
+
+//  const:cannot change or reassign value
+//const a=5;
+//console.log(a+b); 
+//prompt(a+b); ::can input value
+
+//types of data type:
+// number
+//string
+//boolean
+//undefined: when value is not given....
+
+//let test;
+//alert(test);
+
+// == vs ===
+//== :use to check value only
+//=== : use to check value and datatype
+// && 
+//let is=true;
+//let to=true;
+//let t= is && to;
+ //console.log(t);
+
+
+ //let b=[1,2,3,4,5,6,7,8,9]
+//alert(b[5]);
+//let object1={ name:"john",age:"25"};
+//console.log(object1.name,"object1");
+//alert(object1.name,"object1");
+//console.log(object1["age"],"object1");
+//obj[key] or obj.key
+
+let object1={name:"ranjit",age:12,address:"kathmandu"}
+console.log(object1["age"],"object1");
+
