e1262c4b45d218ce4769707fd9131a288c673d42
Rajnit Rai <dipakshrestha475@gmail.com>
2022-12-14 17:49:08 +0545

    added

--- javascript2.js
+++ javascript2.js
@@ -1,18 +1,67 @@
-let object1={
-  name:"Rajnit", 
-   age:18,
-    address:"Kathmandu"}
+//let object1={
+ // name:"Rajnit", 
+  // age:18,
+   // address:"Kathmandu"}
  
 //if (object1.name=="Rajnit" && object1.age>=20){
  //alert('display');
 //}
- if (object1.name=="Rajnit" && object1.age>=20){
-    alert('display');
- }else if (object1.age<19) {
-    alert('show')
- } else {
-    alert('undefined')
- }
+
+//if else if..............................
+ //if (object1.name=="Rajnit" && object1.age>=20){
+   // alert('display');
+ //}else if (object1.age<19) {
+  //  alert('show')
+ //} else {
+ //   alert('undefined')
+ //}
+
+ // Function in javascript...................>
+//function add(parameter1,parameter2){
+
+//}
+//add(parameter1,parameter2);
+
+//function add(a,b){
+// alert(a+b)
+//}
+//add(10,20)
+//add(40,50)
+//...............
+//function mul(a,b){
+   // let c=a*b;
+   // return alert(c)
+//}
+//mul(30,20)
+
+//function cmp(a,b){
+  //  if(a>b){
+  //      alert('display')
+  //  }
+
+//}
+//cmp(50,30)
+
+//function without paramater.................>>>>
+//function cmp(){
+//let a= 20;
+//let b=30;
+//if (a>b){
+  //  alert("wrong")
+    
+//}else{
+ //   alert("right")
+//  }
+//  }
+//  cmp()
+
+//loop ............................
+// let array=[1,2,3,4,5,6,7,8,9];
+// for(let i=0; i<5; i++ ){
+// console.log(i,"print i" );
+// }
+
+
 
 
 
